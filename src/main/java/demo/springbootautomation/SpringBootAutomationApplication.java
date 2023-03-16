ba5097e580fd79ac37d6b9b3616ca8fbbaffbd05
Valeri Sova <valy66666@gmail.com>
2023-03-16 16:09:49 +0200

    first commit

---
+++ src/main/java/demo/springbootautomation/SpringBootAutomationApplication.java
@@ -0,0 +1,13 @@
+package demo.springbootautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/springbootautomation/SpringBootAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.springbootautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
